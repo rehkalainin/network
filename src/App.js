df064522fcefebfcaeb1d63559d45bef3e0da5bc
Konstantin Rehkalainin <rkonstantin@gmail.com>
2020-08-14 16:47:08 +0300

    init network

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header"
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs"
+import {BrowserRouter} from "react-router-dom";
+import Route from "react-router-dom/es/Route";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path="/profile" render={() => <Profile
+                        profilePage={props.state.profilePage}
+                        addPost={props.addPost}
+                        updateText={props.updateText}/>}/>
+                    <Route path="/dialogs" render={() => <Dialogs
+                        dialogPage={props.state.dialogPage}
+                        updateMessage={props.updateMessage}
+                        addMessage={props.addMessage}/>}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+import * as serviceWorker from "./serviceWorker"
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import state, {addMessage, addPost, subscribe, updateMessage, updateText} from "./redux/state"
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let rerender = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateText={updateText}
+                 updateMessage={updateMessage} addMessage={addMessage}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    )
+}
+rerender(state)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+subscribe(rerender)
+
+
+serviceWorker.unregister()
+
